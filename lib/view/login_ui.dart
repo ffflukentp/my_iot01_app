eb96a164733e3c2cb36443975500458bce47cd60
ffflukentp <natthaphonst52@gmail.com>
2024-01-08 21:36:38 +0700

    UI Success

--- lib/view/login_ui.dart
+++ lib/view/login_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_iot01_app/view/home_ui.dart';
 
 class LoginUI extends StatefulWidget {
   const LoginUI({super.key});
@@ -13,14 +14,24 @@ class _LoginUIState extends State<LoginUI> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding:  EdgeInsets.only(
+          padding: EdgeInsets.only(
             left: MediaQuery.of(context).size.width * 0.065,
             right: MediaQuery.of(context).size.width * 0.065,
-            ),
+          ),
+          
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
               IconButton(
-                onPressed: () {Navigator.pop(context);},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeUI()),
+                  );
+                },
                 icon: Icon(
                   Icons.arrow_back_ios,
                 ),
@@ -30,19 +41,133 @@ class _LoginUIState extends State<LoginUI> {
                 height: MediaQuery.of(context).size.height * 0.2,
               ),
               Text(
-                  'Welcome Back,',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: MediaQuery.of(context).size.height * 0.03,
+                'Welcome Back,',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ),
+              Text(
+                'make it work,make it right,make it fast',
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'E-mail',
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 58, 57, 57)))),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.010,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(Icons.fingerprint),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.visibility),
+                    onPressed: () {},
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 58, 57, 57),
+                    ),
                   ),
                 ),
-              Text(
-                  'make it work,make it right,make it fast',
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'forget Password?',
+                    style: TextStyle(color: Colors.cyan),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                  ),
                 ),
-                
-              
-              
-            ],//children
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'OR',
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontSize: MediaQuery.of(context).size.width * 0.04),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    'assets/images/google.png',
+                    width: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  label: Text(
+                    'Sign-In with Google',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Dont have an Account? ',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                  Text(
+                    'Signup',
+                    style: TextStyle(
+                      color: Colors.cyan,
+                    ),
+                  ),
+                ],
+              ),
+            ], //children
           ),
         ),
       ),
--- lib/view/signup_ui.dart
+++ lib/view/signup_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_iot01_app/view/home_ui.dart';
 
 class SignupUI extends StatefulWidget {
   const SignupUI({super.key});
@@ -10,6 +11,177 @@ class SignupUI extends StatefulWidget {
 class _SignupUIState extends State<SignupUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.width * 0.065,
+            right: MediaQuery.of(context).size.width * 0.065,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeUI()),
+                  );
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                ),
+              ),
+              Image.asset(
+                'assets/images/logo.png',
+                height: MediaQuery.of(context).size.height * 0.2,
+              ),
+              Text(
+                'Get On Board!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ),
+              Text(
+                'Create your profile to star your journey.',
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Full Name',
+                    prefixIcon: Icon(Icons.person_outline),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 58, 57, 57)))),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.010,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'E-Mail',
+                  prefixIcon: Icon(Icons.mail_outline),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 58, 57, 57),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.010,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Phone No',
+                  prefixIcon: Icon(Icons.tag),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 58, 57, 57),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.010,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(Icons.fingerprint),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 58, 57, 57),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.019,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'OR',
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontSize: MediaQuery.of(context).size.width * 0.04),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    'assets/images/google.png',
+                    width: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  label: Text(
+                    'Sign-In with Google',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have on Account? ',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                  Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.cyan,
+                    ),
+                  ),
+                ],
+              ),
+            ], //children
+          ),
+        ),
+      ),
+    );
   }
 }
